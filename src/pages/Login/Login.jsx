bc217246cfec924f8467b0556d7e8a5e04143a9d
ljh0608 <m98m@catholic.ac.kr>
2023-06-07 11:07:47 +0900

    fix: login local환경으로 적용

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -11,7 +11,8 @@ import {
 
 const Login = () => {
   const redirectURI =
-    "http://semtle.catholic.ac.kr:3000/allso/auth/kakao_login";
+    // "http://semtle.catholic.ac.kr:3000/allso/auth/kakao_login";
+    "http://localhost:3000/allso/auth/kakao_login";
   const loginOnclick = async () => {
     try {
       loginService.login();
