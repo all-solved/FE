4aaf4113ca76b5b50835909ebdb3bbe5b3f8cbae
ljh0608 <m98m@catholic.ac.kr>
2023-06-26 18:37:17 +0900

    fix: 컴플레인 완료 페이지 초기

--- src/pages/UserCompleteProblem/UserCompleteProblem.jsx
+++ src/pages/UserCompleteProblem/UserCompleteProblem.jsx
@@ -11,10 +11,18 @@ import {
 import UserProblemHeaderCp from "components/UserProblem/UserProblemHeaderCp";
 import UserProblemFooter from "components/UserProblem/UserProblemFooter";
 
-<UserProblemWrapper>
-  <UserProblemLayout>
-    <UserProblemHeaderCp></UserProblemHeaderCp>
-    <UserProblemMainWrapper>컴플레인 접수 완료</UserProblemMainWrapper>
-    <UserProblemFooter></UserProblemFooter>
-  </UserProblemLayout>
-</UserProblemWrapper>;
+import React from "react";
+
+const UserCompleteProblem = () => {
+  return (
+    <UserProblemWrapper>
+      <UserProblemLayout>
+        <UserProblemHeaderCp></UserProblemHeaderCp>
+        <UserProblemMainWrapper>컴플레인 접수 완료</UserProblemMainWrapper>
+        <UserProblemFooter></UserProblemFooter>
+      </UserProblemLayout>
+    </UserProblemWrapper>
+  );
+};
+
+export default UserCompleteProblem;
